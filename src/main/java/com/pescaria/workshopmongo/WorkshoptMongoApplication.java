de7d597ec47b41bf1ae7265c80426961ed5b0188
Crisaoo <Cristianocosta2019@outlook.com>
2020-08-22 17:56:55 -0300

    Created project

---
+++ src/main/java/com/pescaria/workshopmongo/WorkshoptMongoApplication.java
@@ -0,0 +1,13 @@
+package com.pescaria.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoptMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoptMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pescaria/workshopmongo/WorkshoptMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pescaria.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoptMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
